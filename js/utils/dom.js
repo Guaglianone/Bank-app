62e726f29804498c3b845aadf7aa3b6c86606458
Cristian <cristianguaglianone@gmail.com>
2023-01-07 15:51:15 +0100

    starter project

---
+++ js/utils/dom.js
@@ -0,0 +1,34 @@
+// LABELS
+export const labelWelcome = document.querySelector('.welcome');
+export const labelBalance = document.querySelector('.balance__value');
+export const labelSumIn = document.querySelector('.summary__value--in');
+export const labelSumOut = document.querySelector('.summary__value--out');
+export const labelSumInterest = document.querySelector(
+  '.summary__value--interest'
+);
+
+// CONTAINERS
+export const containerApp = document.querySelector('.app');
+export const containerMovements = document.querySelector('.movements');
+export const mainLogin = document.querySelector('.main-login');
+export const formLogin = document.querySelector('form');
+
+// BUTTONS
+export const btnLogin = document.querySelector('.login__btn');
+export const btnLogout = document.querySelector('.logout__btn');
+export const btnTransfer = document.querySelector('.form__btn--transfer');
+export const btnLoan = document.querySelector('.form__btn--loan');
+export const btnClose = document.querySelector('.form__btn--close');
+
+// INPUTS
+export const inputLoginUsername = document.querySelector('.login__input--user');
+export const inputLoginPin = document.querySelector('.login__input--pin');
+export const inputTransferTo = document.querySelector('.form__input--to');
+export const inputTransferAmount = document.querySelector(
+  '.form__input--amount'
+);
+export const inputLoanAmount = document.querySelector(
+  '.form__input--loan-amount'
+);
+export const inputCloseUsername = document.querySelector('.form__input--user');
+export const inputClosePin = document.querySelector('.form__input--pin');
